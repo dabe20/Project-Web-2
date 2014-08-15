439b1d88ed71d80679529f88ce52a2427c49975a
dabe20 <d.a.b.e_20@hotmail.com>
2014-08-15 01:35:54 -0600

    commit 05
    se agregaron nuevos estilos a los elementos html dentro de la pagina,
    viendose de una manera mas elegante y se borraron archivos innecesarios
    para el desarrollo del proyecto.

--- Proyecto2/javascript/editCareer.js
+++
@@ -1,9 +0,0 @@
-function saveEdit() {
-	window.location="Admin_area.html";
-	alert("Cambios guardados exitosamente");
-}
-
-function saveEditNew() {
-	window.location="new_career.html";
-	alert("Cambios guardados exitosamente");
-}
